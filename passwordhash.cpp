67200be55823bb90ea2e499c4b1d155950e0fcb8
JiHounHuh <PatrickWumbo@users.noreply.github.com>
2019-01-14 15:22:18 -0800

    updated thousandLoop

--- passwordhash.cpp
+++ passwordhash.cpp
@@ -4,20 +4,23 @@
 
 using namespace std;
 
-int main() {
-    string Hash = "";
-    string intermediate = "";
-    string sample = "";
-    for(int i = 0; i < 1000; i++) {
-        if(i%2 == 0) sample += intermediate;
-        if(i%2 == 1) sample += password;
-        if(i%3 != 0) sample += salt;
-        if(i%7 != 0) sample += password;
-        if(i%2 == 0) sample += password;
-        if(i%2 == 1) sample += intermediate;
-        //HASH FUNCTION (sample);
-        intermediate = sample;
-        sample = "";
-    }
 
-}
+    string thousandLoop(string password, string interm, string salt, string hash)
+    {
+        string Hash = hash;
+        string intermediate = interm;
+        string sample = "";
+        for(int i = 0; i < 1000; i++) {
+            if(i%2 == 0) sample += intermediate;
+            if(i%2 == 1) sample += password;
+            if(i%3 != 0) sample += salt;
+            if(i%7 != 0) sample += password;
+            if(i%2 == 0) sample += password;
+            if(i%2 == 1) sample += intermediate;
+            //HASH FUNCTION (sample);
+            intermediate = md5(sample);
+            sample = "";
+        }
+        return intermediate;
+
+    }
